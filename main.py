5996bf05699788642c7f03c243b0723533aa2db0
Leonardo Pereira <ethical.leonardo@gmail.com>
2023-09-29 12:19:38 -0300

    adicionado display e label com informações

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 
 from main_window import MainWindow
+from display import Display
+from info import Info
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from variables import WINDOWS_ICON_PATH
@@ -16,6 +18,13 @@ if __name__ == '__main__':
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
 
+    # Info
+    info = Info('2.0 ^ 10.0 = 1024')
+    window.addToVLayout(info)
+
+    # Display
+    display = Display()
+    window.addToVLayout(display)
     # Executa tudo
     window.adjustFixedSize()
     window.show()
