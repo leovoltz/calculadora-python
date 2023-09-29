1ea16380a1a689a0af6441e6194824aab814816a
Leonardo Pereira <ethical.leonardo@gmail.com>
2023-09-29 16:52:10 -0300

    configurado a inserção de texto no display pelos buttons

--- buttons.py
+++ buttons.py
@@ -1,6 +1,8 @@
+from display import Display
 from PySide6.QtWidgets import QPushButton, QGridLayout
-from variables import MEDIUM_FONT_SIZE
+from PySide6.QtCore import Slot
 from utils import isNumOrDot, isEmpty
+from variables import MEDIUM_FONT_SIZE
 
 
 class Button(QPushButton):
@@ -13,10 +15,11 @@ class Button(QPushButton):
         font.setPixelSize(MEDIUM_FONT_SIZE)
         self.setFont(font)
         self.setMinimumSize(75, 75)
+        # self.setCheckable(True)
 
 
 class ButtonsGrid(QGridLayout):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, display: Display, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
@@ -26,6 +29,7 @@ class ButtonsGrid(QGridLayout):
             ['1', '2', '3', '+'],
             ['',  '0', '.', '='],
         ]
+        self.display = display
         self._makeGrid()
 
     def _makeGrid(self):
@@ -37,3 +41,19 @@ class ButtonsGrid(QGridLayout):
                     button.setProperty('cssClass', 'specialButton')
 
                 self.addWidget(button, rowNumber, columnNumber)
+                buttonSlot = self._makeButtonDisplaySlot(
+                    self._insertButtonTextToDisplay,
+                    button,
+                )
+
+                button.clicked.connect(buttonSlot)
+
+    def _makeButtonDisplaySlot(self, func, *args, **kwargs):
+        @Slot(bool)
+        def realSlot(_):
+            func(*args, **kwargs)
+        return realSlot
+
+    def _insertButtonTextToDisplay(self, button):
+        button_text = button.text()
+        self.display.insert(button_text)
